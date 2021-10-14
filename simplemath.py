4e358eeb1cd93b979628281eb5c6e262dfcac97a
Esther Puyol <42403978+estherpuyol@users.noreply.github.com>
2021-10-14 14:52:48 +0200

    Add files via upload

---
+++ simplemath.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 18:53:24 2018
+
+@author: emma
+"""
+
+def mysum(x,y):
+    return x+y
+
+def mult(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
